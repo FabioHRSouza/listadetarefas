2787908919164087f1abb2278f9fb9449e1aafa8
Fabio Souza <fabiohrsouza@gmail.com>
2023-03-23 22:47:44 -0300

    iniciando o imput e a lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useReducer, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+const [textoTarefa, setTextoTarefa] = useState("")
+
+const [listaDeTarefas, dispatchListaDeTarefas] = useReducer(reducerTarefas, [])
+
+function reducerTarefas (state, action) {
+
+  switch (action.type) {
+      case "ADICIONAR":
+        let novaTarefa = {
+          id: state.length + 1,
+          texto: textoTarefa,
+          finalizado: false,
+        }
+        setTextoTarefa("")
+        return [...state, novaTarefa]
+        
+      case "CONCLUIR":
+        break;
+  }
+}
+
+return (
+  <div>
+    <div>
+    <input  
+    placeholder="Texto da Tarefa" 
+    type="text"
+    value={textoTarefa} 
+    onChange={(evento) => setTextoTarefa(evento.target.value)} 
+    /> 
+    <button onClick={() => dispatchListaDeTarefas({ type: "ADICIONAR" })}>Adicionar</button> 
     </div>
-  )
+    <div>
+      {
+        listaDeTarefas.map(tarefa => (
+          <li key={tarefa.id}>{tarefa.texto}</li>
+        ))
+      }
+    </div>
+  </div>
+)
+
 }
 
 export default App
---
+++ src/Components/CardAdicionar.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function CardAdicionar(props) {
+
+
+
+    <input
+    placeholder="adicionar tarefa"
+    type="text"
+    value={//value aqui}
+    onChange={//função aqui}
+    />
+
+    }
