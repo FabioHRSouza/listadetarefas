ae816c0734c68000deb152cbbb272bd50ad6ad7d
Fabio Souza <fabiohrsouza@gmail.com>
2023-04-02 10:41:58 -0300

    Finalizado a semana 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,46 @@
-import { useReducer, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
-import React from 'react'
-import { ToDoProvider } from './context/ToDoProvider'
-import { ToDoList } from './Components/ToDoList'
+import CardAdicionar from './components/CardAdicionar'
+import ListItem from './components/ListItem'
 
 function App() {
 
-  return (
-    <ToDoProvider>
-      <ToDoList />
-    </ToDoProvider>
-  )
+  // estado base
+  const [listaDeTarefas, setListaDeTarefas] = useState([
+    {id: 1, texto: "Tarefa Antiga", finalizado: false}
+  ])
+  
+  function adicionarTarefa(tarefa){
+    /* 
+      1 [x] receber o texto da tarefa
+      2 [x] criar o objeto tarefa
+      3 [x] set na lista de tarefas*
+        3.1 [x] criar uma nova lista com todas as tarefas
+        3.2 [x] passar a nova lista para o set
+    */
+    const novaTarefa = {id: listaDeTarefas.length + 1, texto: tarefa, finalizado: false}
 
-}
+    setListaDeTarefas([...listaDeTarefas, novaTarefa])
+  }
 
-export default App
+  function removerTarefa(tarefa){
 
+    // filtra a tarefa selecionada e assim remove a tarefa
+    const novaLista = listaDeTarefas.filter(item => item.id != tarefa.id)
 
-/*if (acao.tipo === "atualizar data") {
-  novoEstado = {
-    ...estado,
-    data_alteracao: new Date().toLocaleDateString("en-US"),
-  };
-}
-
-import { useState } from "react";
-import "./ListItem.css";
-import { MdThumbUp, MdThumbDown } from "react-icons/md";
-
-function ListItem({ tarefa }) {
-  const [finalizado, setFinalizado] = useState(tarefa.finalizado);
-
-  function finalizarTarefa() {
-    setFinalizado(true);
+    setListaDeTarefas(novaLista)
   }
 
   return (
-    <div className="list-item">
-      <p className={finalizado ? "finalizado sem-borda" : ""}>{tarefa.texto}</p>
-      {finalizado ? (
-        <MdThumbUp />
-      ) : (
-        <button className="button-reset" o
+    <div className="App">
+      <CardAdicionar adicionarTarefa={adicionarTarefa} />
+      {
+        listaDeTarefas.map(tarefa => (
+          <ListItem tarefa={tarefa} removerTarefa={removerTarefa}/>
+        ))
+      }
+    </div>
+  )
+}
 
-*/
+export default App
